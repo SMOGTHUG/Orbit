879296bda93fb025d2c47c10ab755b88886fc2ca
Adedamola <45236923+dahmaular@users.noreply.github.com>
2024-08-19 13:11:33 +0100

    added ai codes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Home from './components/home/Home';
+import HomePage from './orbitfs/components/HomePage'
 
 const App = () => {
   return (
     <Router>
     <Routes>
-      <Route path='/' element={<Home />}/>
+      <Route path='/' element={<HomePage />}/>
     </Routes>
    </Router>
   )
